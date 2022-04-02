b3e62403cd657a9a3f05a78b1ad9ab672aedb061
Philipp Proksch <mail@prp.wien>
2022-04-02 20:24:55 +0200

    ✨ Added: Bill, People, Tip and Total components

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,20 @@
-import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import { useState } from 'react';
+import logo from './logo.svg';
+import './App.scss';
+import Bill from '../components/Bill';
+import Tip from '../components/Tip';
+import People from '../components/People';
+import Total from '../components/Total';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.jsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
+      <Bill />
+      <Tip />
+      <People />
+      <Total />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
